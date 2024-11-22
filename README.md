f94fe901a02f4a1fc1a40421174d78f540bd80fe
widiskel <waputra16@gmail.com>
2024-11-22 21:06:30 +0700

    fix (unlock worm) fixing unlock worm status, and add skip common worm when unlocking

--- README.md
+++ README.md
@@ -193,6 +193,7 @@ export class Config {
   static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
   static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf" WATCH THE ""
   static USEONCHAINTX = false; // SET To TRUE IF YOU WANT TO USE ONCHAIN TX FOR CHECK IN AND UPGRADE
+  static SKIPCOMMONWORM = true; // SET TO TRUE IF YOU DONT WANT TO UNLOCKING WORM WITH COMMON RARITY
   static USERWALLET = [
     [
       "YOUR SUI PK EX: 'suiprivkeyxxxx'",
--- config/config_tmp.js
+++ config/config_tmp.js
@@ -1,15 +1,13 @@
 export class Config {
   static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
   static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
-  static USEONCHAINTX = false; // SET To TRUE IF YOU WANT TO USE ONCHAIN TX FOR CHECK IN AND UPGRADE
+  static USEONCHAINTX = false; // SET TO TRUE IF YOU WANT TO USE ONCHAIN TX FOR CHECK IN AND UPGRADE
+  static SKIPCOMMONWORM = true; // SET TO TRUE IF YOU DONT WANT TO UNLOCKING WORM WITH COMMON RARITY
   static USERWALLET = [
+    ["suiprivkeyxxx", "accounts/query-akun1"],
     [
-      "YOUR SUI PK EX: 'suiprivkeyxxxx'",
-      "YOUR ACCOUNTS FOLDER EX: 'accounts/query-akun1",
-    ],
-    [
-      "YOUR SUI PK EX: 'suiprivkeyxxxx'",
-      "YOUR ACCOUNTS FOLDER EX: 'accounts/query-akun2",
+      "YOUR SUI PK EX: suiprivkeyxxxx",
+      "YOUR ACCOUNTS FOLDER EX: accounts/query-akun2",
     ],
   ];
 }
